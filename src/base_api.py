165aaddb4ca87c08d0f432e1ec7f16fe24f3bf75
Oleg Silkin <o.silkin98@gmail.com>
2018-09-14 23:07:46 -0500

    Adds printing of requests for verbose debugging

--- src/base_api.py
+++ src/base_api.py
@@ -61,3 +61,23 @@ class BaseApi(object):
 
         except requests.HTTPError as HE:
             print(HE)
+
+        except requests.RequestException as RE:
+            print
+
+
+def pretty_print_POST(req):
+    """
+    At this point it is completely built and ready
+    to be fired; it is "prepared".
+
+    However pay attention at the formatting used in
+    this function because it is programmed to be pretty
+    printed and may differ from the actual request.
+    """
+    print('{}\n{}\n{}\n\n{}'.format(
+        '-----------START-----------',
+        req.method + ' ' + req.url,
+        '\n'.join('{}: {}'.format(k, v) for k, v in req.headers.items()),
+        req.body,
+    ))
