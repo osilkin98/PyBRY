8b63d4dd6932942e0d527f87c96877b8b273b3c9
Oleg Silkin <o.silkin98@gmail.com>
2018-09-15 01:42:05 -0500

    Adds setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='PyBRY',
+    version='1.0',
+    packages=['src',],
+    license='MIT License',
+    long_description=open('README.md', mode='r').read()
+)
