388bf1414c892d31e95a6e433481fe6205ab0612
Oleg Silkin <o.silkin98@gmail.com>
2018-09-15 00:20:45 -0500

    Adds call function

--- src/lbrycrd_api.py
+++ src/lbrycrd_api.py
@@ -12,3 +12,20 @@ class LbrycrdApi(BaseApi):
         :param float timeout: Number of seconds before we give up on waiting for server to respond
         """
 
+        self.basic_auth = (username, password)
+        self.timeout = timeout
+
+
+    def call(self, method, params=None):
+        """
+
+        :param str method: Method to call from lbrycrd. To view the full list of methods, run ./lbrycrd-cli help,
+         or ./lbrycrd-cli [command_name] help for information on a specific command
+         https://lbryio.github.io/lbry/cli/
+        :param dict params: Parameters to give the method selected
+        :raises LBRYException: If the request returns an error when calling the API
+        :return: A Python `dict` object containing the data requested from the API
+        :rtype: dict
+        """
+
+        return self.make_request(LBRYCRD_SERVER_ADDRESS, method, params, self.basic_auth, self.timeout)
