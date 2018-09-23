3f16612994bbd73bf28cb3f9806ecee336247fda
Oleg Silkin <o.silkin98@gmail.com>
2018-09-23 01:05:50 -0500

    Modifies the call method for LbryAPI to be a class method

--- pybry/lbry_api.py
+++ pybry/lbry_api.py
@@ -10,12 +10,14 @@ class LbryApi(BaseApi):
         """
         self.timeout = timeout
 
-    def call(self, method, params=None):
+    @classmethod
+    def call(cls, method, params=None, timeout=600):
         """ Makes a Call to the LBRY API
 
         :param str method: Method to call from the LBRY API. See the full list of methods at
          https://lbryio.github.io/lbry/cli/
         :param dict params: Parameters to give the method selected
+        :param float timeout: The number of seconds to wait for a connection until we time out; 600 By Default.
         :raises LBRYException: If the request returns an error when calling the API
         :return: A Python `dict` object containing the data requested from the API
         :rtype: dict
@@ -23,4 +25,4 @@ class LbryApi(BaseApi):
 
         params = [] if params is None else params
 
-        return self.make_request(LBRY_SERVER_ADDRESS, method, params, timeout=self.timeout)
+        return cls.make_request(LBRY_SERVER_ADDRESS, method, params, timeout=timeout)
