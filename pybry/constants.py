8f0278418968cd9771213ab677d01cef2c549b5a
Oleg Silkin <o.silkin98@gmail.com>
2018-09-22 23:50:35 -0500

    Adds the links for lbry api docs

--- pybry/constants.py
+++ pybry/constants.py
@@ -3,3 +3,9 @@
 # Defines server constants so they dont get altered by other files
 LBRY_SERVER_ADDRESS = "http://localhost:5279"
 LBRYCRD_SERVER_ADDRESS = "http://localhost:9245"
+
+# This is the URL for the lbryd API documentation
+LBRYD_API_RAW_JSON_URL = "https://raw.githubusercontent.com/lbryio/lbry/master/docs/api.json"
+
+# LBRYCRD documentation doesn't exist at least that I could find
+# LBRYCRD_API_RAW_JSON_URL = ""
