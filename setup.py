c7a77b9d9c6df2250d5eeca7d72a05067cc5ce0d
belikor <belikor@users.noreply.github.com>
2021-09-26 13:11:35 -0500

    setup.py: update the `cmdclass` to build the project and API wrapper
    The `setup.py` file uses the `setuptools` library to build
    the project and generate the API wrapper `pybry/lbryd_api.py`.
    
    However, since `setup.py` imports `pybry`, and this imports
    the existing `lbryd_api.py`, the entire `setup.py` may fail
    if `lbryd_api.py` has syntax errors.
    For this reason, using the `Makefile` is preferred.

--- setup.py
+++ setup.py
@@ -2,38 +2,58 @@ from setuptools import setup
 from generator import generate_lbryd_wrapper
 from os import path
 from setuptools.command.build_py import build_py
+# This should be replaced by setuptools as distutils is deprecated
+from distutils.command.clean import clean
+from distutils.file_util import copy_file
 from pybry import __version__
 
 NAME = 'pybry'
 
 
-class generate_on_build(build_py):
-    """ Generates the actual file for release at build time """
+class GenerateAPILocalJSON(build_py):
+    """Generates the API from the local JSON file."""
+
+    def run(self):
+        generate_lbryd_wrapper(doc="docs/api.json")
+        build_py.run(self)
+
+
+class GenerateAPIURLJSON(build_py):
+    """Generates the API from the JSON file in the online repository."""
 
     def run(self):
         generate_lbryd_wrapper()
         build_py.run(self)
 
 
+class Clean(clean):
+    def run(self):
+        copy_file("pybry/__lbryd_api__.py", "pybry/lbryd_api.py")
+        clean.run(self)
+
+
 # To read markdown file
 this_directory = path.abspath(path.dirname(__file__))
 
 with open(path.join(this_directory, 'README.md'), mode='r', encoding='utf-8') as outfile:
-    long_description=outfile.read()
+    long_description = outfile.read()
 
 setup(
-    name='pybry',
-    url='https://github.com/osilkin98/pybry',
-    author='Oleg Silkin',
+    name=NAME,
+    url='https://github.com/belikor/PyBRY',
+    author='Oleg Silkin, Beliko R',
     author_email='o.silkin98@gmail.com',
     version=__version__,
-    py_modules=[NAME, ],
+    packages=[NAME],
     license='MIT License',
     description="A Binded Python API for the LBRYD and LBRYCRD network",
     long_description=long_description,
     long_description_content_type='text/markdown',
     requires=['yapf'],
     python_requires='>=3',
-    cmdclass={'build_py': generate_on_build}
-
+    cmdclass={'build_py': GenerateAPILocalJSON,
+              'build_local': GenerateAPILocalJSON,
+              'build_online': GenerateAPIURLJSON,
+              'clean': Clean}
 )
+
